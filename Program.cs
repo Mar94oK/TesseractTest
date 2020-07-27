69a1a1c6ede6074b7265edb9d0a145edd534659e
Mar94oK <h.uvarenkov@gmail.com>
2020-07-27 20:26:11 +0300

    Intial

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tesseract;
+
+using System.Drawing;
+
+using System.IO; // For Path and File
+
+namespace TesseractTest
+{
+    class Program
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+
+
+            var testImagePath = "./phototest.tif";
+            if (args.Length > 0)
+            {
+                testImagePath = args[0];
+            }
+
+            //https://www.cyberforum.ru/csharp-net/thread320443.html
+            //https://stackoverflow.com/questions/14265492/how-to-get-path-of-properties-resources-image-in-net/14265535#14265535
+            //File.WriteAllBytes("newFile.xls", WindowsFormsApplication1.Properties.Resources.myFile);
+
+            Logger.Debug("Getting eng language package from resources and save it to temporary folder");
+            var path = @"./";
+            Logger.Debug("Temporary path: " + path);
+            File.WriteAllBytes(path + "eng.traineddata", Resource_LanguageData.eng);
+
+            try
+            {
+                //using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                using (var engine = new TesseractEngine(path, "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(testImagePath))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            var text = page.GetText();
+                            Console.WriteLine("Mean confidence: {0}", page.GetMeanConfidence());
+
+                            Console.WriteLine("Text (GetText): \r\n{0}", text);
+                            Console.WriteLine("Text (iterator):");
+                            using (var iter = page.GetIterator())
+                            {
+                                iter.Begin();
+
+                                do
+                                {
+                                    do
+                                    {
+                                        do
+                                        {
+                                            do
+                                            {
+                                                if (iter.IsAtBeginningOf(PageIteratorLevel.Block))
+                                                {
+                                                    Console.WriteLine("<BLOCK>");
+                                                }
+
+                                                Console.Write(iter.GetText(PageIteratorLevel.Word));
+                                                Console.Write(" ");
+
+                                                if (iter.IsAtFinalOf(PageIteratorLevel.TextLine, PageIteratorLevel.Word))
+                                                {
+                                                    Console.WriteLine();
+                                                }
+                                            } while (iter.Next(PageIteratorLevel.TextLine, PageIteratorLevel.Word));
+
+                                            if (iter.IsAtFinalOf(PageIteratorLevel.Para, PageIteratorLevel.TextLine))
+                                            {
+                                                Console.WriteLine();
+                                            }
+                                        } while (iter.Next(PageIteratorLevel.Para, PageIteratorLevel.TextLine));
+                                    } while (iter.Next(PageIteratorLevel.Block, PageIteratorLevel.Para));
+                                } while (iter.Next(PageIteratorLevel.Block));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //Trace.TraceError(e.ToString());
+                Console.WriteLine("Unexpected Error: " + e.Message);
+                Console.WriteLine("Details: ");
+                Console.WriteLine(e.ToString());
+            }
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+
+        }
+    }
+}
